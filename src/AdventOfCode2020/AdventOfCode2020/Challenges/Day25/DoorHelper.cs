143a424d8f40db8ffab9dee753ccb80d06f7bafa
erjicles <erjicles@gmail.com>
2020-12-25 13:50:51 -0600

    Completed Day 25 - Part 01

---
+++ src/AdventOfCode2020/AdventOfCode2020/Challenges/Day25/DoorHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Challenges.Day25
+{
+    public static class DoorHelper
+    {
+        public static BigInteger GetEncryptionKey(
+            BigInteger publicKeyDoor, 
+            BigInteger publicKeyCard)
+        {
+            var loopSizeDoor = GetLoopSizeFromPublicKey(publicKeyDoor);
+            Console.WriteLine($"Door loop size: {loopSizeDoor}");
+            var loopSizeCard = GetLoopSizeFromPublicKey(publicKeyCard);
+            Console.WriteLine($"Card loop size: {loopSizeCard}");
+
+            var encryptionKeyDoor = GetTransformedSubjectNumber(loopSizeDoor, publicKeyCard);
+            Console.WriteLine($"Encryption key (door): {encryptionKeyDoor}");
+            var encryptionKeyCard = GetTransformedSubjectNumber(loopSizeCard, publicKeyDoor);
+            Console.WriteLine($"Encryption key (card): {encryptionKeyCard}");
+
+            if (encryptionKeyCard != encryptionKeyDoor)
+            {
+                throw new Exception($"Encryption keys don't match: card: {encryptionKeyCard}, door: {encryptionKeyDoor}");
+            }
+
+            return encryptionKeyCard;
+        }
+
+        public static int GetLoopSizeFromPublicKey(BigInteger publicKey)
+        {
+            int result = 1;
+            BigInteger currentTransform = 1;
+            while (true)
+            {
+                currentTransform = (currentTransform * 7) % 20201227;
+                if (currentTransform == publicKey)
+                {
+                    break;
+                }
+                result++;
+            }
+            return result;
+        }
+
+        public static BigInteger GetTransformedSubjectNumber(int loopSize, BigInteger subjectNumber)
+        {
+            BigInteger result = 1;
+            for (int i = 0; i < loopSize; i++)
+            {
+                result = (result * subjectNumber) % 20201227;
+            }
+            return result;
+        }
+    }
+}
--- src/AdventOfCode2020/AdventOfCode2020Test/Challenges/Day25Test.cs
+++ src/AdventOfCode2020/AdventOfCode2020Test/Challenges/Day25Test.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using AdventOfCode2020.Challenges.Day25;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AdventOfCode2020Test.Challenges
 {
     public class Day25Test
     {
+        [Fact]
+        public void GetLoopSizeFromPublicKeyTest()
+        {
+            var testData = new List<Tuple<BigInteger, int>>()
+            {
+                new Tuple<BigInteger, int>(
+                    5764801,
+                    8),
+                new Tuple<BigInteger, int>(
+                    17807724,
+                    11)
+            };
+
+            foreach (var testExample in testData)
+            {
+                var actual = DoorHelper.GetLoopSizeFromPublicKey(testExample.Item1);
+                Assert.Equal(testExample.Item2, actual);
+            }
+        }
+
+        [Fact]
+        public void GetEncryptionKeyTest()
+        {
+            var testData = new List<Tuple<BigInteger, BigInteger, BigInteger>>()
+            {
+                new Tuple<BigInteger, BigInteger, BigInteger>(
+                    17807724,
+                    5764801,
+                    14897079)
+            };
+
+            foreach (var testExample in testData)
+            {
+                var actual = DoorHelper.GetEncryptionKey(testExample.Item1, testExample.Item2);
+                Assert.Equal(testExample.Item3, actual);
+            }
+        }
+
+        [Fact]
+        public void GetDay25Part01AnswerTest()
+        {
+            BigInteger expected = 19414467;
+            BigInteger actual = Day25.GetDay25Part01Answer();
+            Assert.Equal(expected, actual);
+        }
     }
 }
